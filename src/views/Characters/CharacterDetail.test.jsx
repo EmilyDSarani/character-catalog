1d29e4868c31c314410ba02f347775d2168c85ad
EmilyDSarani <emily.sarani@gmail.com>
2022-01-10 14:02:25 -0600

    commit coding as to why mocking is important

--- src/views/Characters/CharacterDetail.test.jsx
+++ src/views/Characters/CharacterDetail.test.jsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import CharacterDetail from './CharacterDetail'
+//mock out the testing so that when internet fails, your tests do not. 
+//this is because this test is directly hitting the API
+
 
 describe('Character Detail Page', () => {
     it('should render a character', async () => {
