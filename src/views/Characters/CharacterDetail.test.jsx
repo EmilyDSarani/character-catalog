f2658216eeb8678273c23ab366b66ed8b02139c5
EmilyDSarani <emily.sarani@gmail.com>
2022-01-10 14:23:42 -0600

    CharacterDetail passing, the test they gave to us is failing though

--- src/views/Characters/CharacterDetail.test.jsx
+++ src/views/Characters/CharacterDetail.test.jsx
@@ -1,14 +1,15 @@
-import { render, screen } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
-import CharacterDetail from './CharacterDetail'
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+import CharacterDetail from './CharacterDetail';
 //mock out the testing so that when internet fails, your tests do not. 
 //this is because this test is directly hitting the API
 
 
 const server = setupServer(
-    rest.get ('https://rickandmortyapi.com/api/character/id', (req, res, ctx) => {
+    rest.get ('https://rickandmortyapi.com/api/character/:id', (req, res, ctx) => {
         return res(
             ctx.json({
                 image: 'https://place-puppy.com/',
@@ -21,16 +22,22 @@ const server = setupServer(
 );
 
 describe('Character Detail Page', () => {
+    beforeAll(() => {
+        server.listen();
+    });
+    afterAll(() => {
+        server.close();
+    });
+
     it('should render a character', async () => {
         render(
             <MemoryRouter initialEntries={['/characters/1']}>
-                <Route>
-                <CharacterDetail />
-                </Route>
+                <Route path="/characters/:id">
+               <CharacterDetail />
+               </Route>
             </MemoryRouter>
         );
-
-        screen.getByText('Loading character')
-        screen.findByText('Rick Sanchez');
+        screen.getByText('Loading character...')
+        await screen.findByText('Rick');
     } )
 })
